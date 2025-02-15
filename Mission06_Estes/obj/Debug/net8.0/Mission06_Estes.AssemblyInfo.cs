c5499aadc47617b1749006e7253ba5c580d963b1
Jack Estes <jackestes3210@gmail.com>
2025-02-15 15:09:20 -0700

    Done

--- Mission06_Estes/obj/Debug/net8.0/Mission06_Estes.AssemblyInfo.cs
+++ Mission06_Estes/obj/Debug/net8.0/Mission06_Estes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission06_Estes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fe7defa9d0caedfa16214eae48e9bff74eb155d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+960c1d03f7071ebb3c54bc975dcaf58bd144dcb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission06_Estes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission06_Estes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
